1ae59750b72ee2b79eb39cd6dbf36103e8f7c635
rezatajari <reza.tajari70@gmail.com>
2020-05-16 15:19:52 +0430

    the band
    -design navbar and set script it
    -design slideshow images in slideshow section

--- 1-the band/script/main.js
+++ 1-the band/script/main.js
@@ -1,6 +1,22 @@
 let touch = document.querySelector('.touch');
+let nav = document.querySelector('.nav');
 
-touch.addEventListener('click', function() {
-    console.log('salam');
 
+touch.addEventListener('click', function(e) {
+    nav.classList.toggle('navshow');
 })
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let imageSlideShow = document.querySelectorAll('.image_slideshow');
+    for (let i = 0; i < imageSlideShow.length; i++) {
+        imageSlideShow[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > imageSlideShow.length) { slideIndex = 1 }
+    imageSlideShow[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
