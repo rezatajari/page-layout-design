a53a0c91527814679baf12e59625205362e124de
rezatajari <reza.tajari70@gmail.com>
2020-05-20 19:54:59 +0430

    the band
    fix some problem.
    -touch and go to section by nav
    -fix script hide nav after click on nav link
    -fixed header for all webpage

--- 1-the band/script/main.js
+++ 1-the band/script/main.js
@@ -1,10 +1,19 @@
 let touch = document.querySelector('.touch');
+let hideNavByNavLink = document.getElementsByClassName('hnav');
 let nav = document.querySelector('.nav');
 
 
 touch.addEventListener('click', function(e) {
     nav.classList.toggle('navshow');
 })
+
+for (let i = 0; i < hideNavByNavLink.length; i++) {
+
+    hideNavByNavLink[i].addEventListener('click', function() {
+        nav.classList.toggle('navshow');
+    })
+
+}
 
 
 var slideIndex = 0;
